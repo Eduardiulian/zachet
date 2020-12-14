3ec61601d7f563b4f53b16894d7da180cbba3f3c
Labzunova <dasha.labzunova@mail.ru>
2020-12-15 01:19:12 +0300

    localization done

--- project/app/controllers/application_controller.rb
+++ project/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include SessionHelper
+  before_action :set_locale
 
   def current_user
     @_current_user ||= session[:current_user_id] &&
@@ -10,5 +11,13 @@ class ApplicationController < ActionController::Base
 
   def authenticate
     redirect_to signin_path unless current_user
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_ortions(options = {})
+    { locale: I18n.locale }.merge option
   end
 end
--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'session/new'
-  get 'session/login'
-  post 'session/create'
-  root 'main#index'
-  get 'main/index'
-  get 'main/profile'
-  post 'main/edit'
-  get 'main/profile_edit'
-  get '/signin', to: 'session#new'
-  get '/signup', to: 'users#new'
-  get '/logout', to: 'session#logout'
-  resources :users
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    get 'users/new'
+    get 'session/new'
+    get 'session/login'
+    post 'session/create'
+    root 'main#index'
+    get 'main/index'
+    get 'main/profile'
+    post 'main/edit'
+    get 'main/profile_edit'
+    get '/signin', to: 'session#new'
+    get '/signup', to: 'users#new'
+    get '/logout', to: 'session#logout'
+    resources :users
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
